cb17a59a39ffdbc389e2bfb41140000e55e44288
Nikolas <nikolas9-cool@ya.ru>
2016-04-23 22:05:16 +0300

    after install 23/04/16

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://magazin/admin/');
+define('HTTP_CATALOG', 'http://magazin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://magazin/admin/');
+define('HTTPS_CATALOG', 'http://magazin/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/OpenServer/domains/magazin/admin/');
+define('DIR_SYSTEM', 'E:/OpenServer/OpenServer/domains/magazin/system/');
+define('DIR_LANGUAGE', 'E:/OpenServer/OpenServer/domains/magazin/admin/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/OpenServer/domains/magazin/admin/view/template/');
+define('DIR_CONFIG', 'E:/OpenServer/OpenServer/domains/magazin/system/config/');
+define('DIR_IMAGE', 'E:/OpenServer/OpenServer/domains/magazin/image/');
+define('DIR_CACHE', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/upload/');
+define('DIR_CATALOG', 'E:/OpenServer/OpenServer/domains/magazin/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'magazin');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://magazin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://magazin/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/OpenServer/domains/magazin/catalog/');
+define('DIR_SYSTEM', 'E:/OpenServer/OpenServer/domains/magazin/system/');
+define('DIR_LANGUAGE', 'E:/OpenServer/OpenServer/domains/magazin/catalog/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/OpenServer/domains/magazin/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/OpenServer/OpenServer/domains/magazin/system/config/');
+define('DIR_IMAGE', 'E:/OpenServer/OpenServer/domains/magazin/image/');
+define('DIR_CACHE', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/OpenServer/domains/magazin/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'magazin');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
